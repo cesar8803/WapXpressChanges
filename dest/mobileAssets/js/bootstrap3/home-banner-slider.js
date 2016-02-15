ff0d9883c44fc42f725a2fb87581ee9f8e190bc7
Jonathan Romero <[jonathan.jjrt@gmail.com]>
2016-02-15 15:31:48 -0600

    Cambios footer, agregue sprite css

---
+++ dest/mobileAssets/js/bootstrap3/home-banner-slider.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.baner-content').bxSlider({
+  	minSlides: 3,
+  	maxSlides: 1,
+  	auto:true,
+  	slideWidth: 0,
+  	slideMargin: 0,
+  	slidePadding:0
+  });
+});
