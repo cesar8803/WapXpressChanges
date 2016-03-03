8177022f07ff03014ba1b46746f33c355213a93b
Jonathan Romero <[jonathan.jjrt@gmail.com]>
2016-03-02 21:39:43 -0600

    Agregue la vista para filtros completa revisar

---
+++ dest/mobileAssets/js/bootstrap3/filtros/filtros.js
@@ -0,0 +1,79 @@
+
+    // add the animation to the popover
+    $('a[rel=popover]').popover().click(function(e) {
+        e.preventDefault();        
+         var open = $(this).attr('data-easein');
+        if(open == 'shake') {
+                  $(this).next().velocity('callout.' + open);
+            } else if(open == 'pulse') {
+              $(this).next().velocity('callout.' + open);
+            } else if(open == 'tada') {
+                $(this).next().velocity('callout.' + open);
+            } else if(open == 'flash') {
+                  $(this).next().velocity('callout.' + open);
+            }  else if(open == 'bounce') {
+                 $(this).next().velocity('callout.' + open);
+            } else if(open == 'swing') {
+                 $(this).next().velocity('callout.' + open);
+            }else {
+             $(this).next().velocity('transition.' + open); 
+            }
+      
+      
+                
+    });
+
+
+   // add the animation to the modal
+$( ".modal" ).each(function(index) {
+   $(this).on('show.bs.modal', function (e) {
+ var open = $(this).attr('data-easein');
+     if(open == 'shake') {
+                 $('.modal-dialog').velocity('callout.' + open);
+            } else if(open == 'pulse') {
+                 $('.modal-dialog').velocity('callout.' + open);
+            } else if(open == 'tada') {
+                 $('.modal-dialog').velocity('callout.' + open);
+            } else if(open == 'flash') {
+                 $('.modal-dialog').velocity('callout.' + open);
+            }  else if(open == 'bounce') {
+                 $('.modal-dialog').velocity('callout.' + open);
+            } else if(open == 'swing') {
+                 $('.modal-dialog').velocity('callout.' + open);
+            }else {
+              $('.modal-dialog').velocity('transition.' + open);
+            }
+             
+}); 
+});
+
+$(".cerrar-filtros").on("click",function(){
+
+$(".modal-fullscreen").velocity({right:"-100%"});
+$(".modal-dialog").velocity({right:"-100%"});
+$(".modal-backdrop .in").velocity({right:"-100%"});
+
+$(".modal-content").velocity({right:"-100%"},function(){
+    $('#modalFiltros').modal('toggle');
+    $(".modal-fullscreen").velocity({right:"0%"});
+    $(".modal-dialog").velocity({right:"0%"});
+    $(".modal-content").velocity({right:"0%"});
+});
+/*$(".modal-fullscreen").removeClass("in");
+$(".modal-dialog").removeClass("in");
+$(".modal-content").removeClass("in");
+$(".modal-backdrop").removeClass("in");*/
+
+});
+
+$('.filtros-secundario h3').on("click", function() {
+if ($(this).find("a").hasClass('active-filter')) {
+$(this).parent().find("ul").slideToggle(200);
+$(this).find("a").removeClass('active-filter');
+}else{
+   $('.filtros-secundario ul').slideUp(200);
+   $('.filtros-secundario h3 a').removeClass('active-filter');
+   $(this).parent().find("ul").slideToggle(200);
+   $(this).find("a").addClass('active-filter');
+}
+});
